1fc84e77c781d09cd2aca79ce9a7ade3241803e9
tonic <curciotonic@gmail.com>
2026-05-12 17:54:41 +0200

    first commit 12 du 05

---
+++ Graphics.js
@@ -0,0 +1,34 @@
+
+
+export class Graphics{
+
+
+constructor(){
+
+this.cnv=document.getElementById("game");
+this.ctx=this.cnv.getContext("2d");
+
+}
+
+
+toConfigWin(){
+
+    console.log('function run');
+console.log("cnv :" , this.cnv.width);
+
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
---
+++ MainProgramme.js
@@ -0,0 +1,11 @@
+
+import {Graphics} from "./Graphics.js";
+
+
+let graphics =new Graphics();
+
+graphics.toConfigWin();
+
+
+
+
