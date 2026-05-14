b709976d68c41d6755e20c3294af727e969a9fbe
tonic <curciotonic@gmail.com>
2026-05-14 19:19:40 +0200

    debut de logique de la commande pc

--- Deplacement.js
+++ Deplacement.js
@@ -5,9 +5,14 @@ export class Deplacement{
   constructor(){
     
     this.dx=20;
-    this.dy;
+    this.dy=20;
     this.posX=0;
     this.posY=0;
+    this.keyDownIsPushed=false;
+    this.keyUpIsPushed=false;
+    this.keyLeftIsPushed=false;
+    this.keyRightIsPushed=false;
+    this.pcControlEvent();
    
    
    
@@ -33,6 +38,39 @@ export class Deplacement{
       this.posX=0;
       
     }
+
+
+// down direction
+
+    if(this.keyDownIsPushed){
+if(this.posY<400){
+
+this.dx=0;
+this.posY+=this.dy;
+
+}else{
+  this.posY=0;
+}
+
+}
+
+
+if(this.keyRightIsPushed){
+
+  this.dy=0;
+  this.dx=20;
+this.posX++;
+
+}
+
+if(this.keyLeftIsPushed){
+
+  this.dx=-this.dx;
+  this.posX+=this.dx;
+
+}
+
+
     
    //requestAnimationFrame(this.toMove);
   }
@@ -43,12 +81,78 @@ export class Deplacement{
 //controle mobile 
 
 
+  // controle pc 
+  pcControlEvent(){
+
+  document.addEventListener("keydown", (e) => {
+
+this.toPushdown(e);
+
+});
+
+
+
+document.addEventListener('keyup', (e) => {
+
+this.toPushUp(e);
+
+
+});
+
+
   
+}
+
+
+toPushdown(e){  
+  // console.log("key push :", e.key);
+
+switch(e.key){
+
+
+case 'ArrowDown': this.keyDownIsPushed=true;
+  break;
+  case 'ArrowLeft': this.keyLeftIsPushed=true;
+  break;
+  case 'ArrowUp': this.keyUpIsPushed=true;
+  break;
+case 'ArrowRight': this.keyRightIsPushed=true;
+  break;
+
+}
+
+
   
-  
-  
-  
-  
+}
+
+
+
+toPushUp(e){
+
+  // console.log("up",e);
+
+switch(e.key){
+
+case 'ArrowDown':this.keyUpIsPushed=false;
+break;
+ case 'ArrowLeft': this.keyLeftIsPushed=false;
+  break;
+  case 'ArrowUp': this.keyDownIsPushed=false;
+  break;
+case 'ArrowRight': this.keyLeftIsPushed=false;
+  break;
+
+
+}
+
+
+}
+
+
+
+
+
+
 }
 
 
